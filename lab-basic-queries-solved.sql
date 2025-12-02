308d2722804d0351e962c53c960340d72f699a0c
Alexandra.Diaconita <alexandradiaconita102@gmail.com>
2025-12-02 21:23:52 +0000

    Lab solved. Resubmission

--- lab-basic-queries-solved.sql
+++ lab-basic-queries-solved.sql
@@ -1,6 +1,7 @@
 -- 1 Display all available tables 
 
 USE sakila;
+SHOW TABLES FROM sakila;
 
 -- 2 Retrieve all the data from actor, film, customer:
 SELECT * FROM actor;
@@ -15,16 +16,16 @@ SELECT first_name from sakila.staff;
 -- 4
 SELECT DISTINCT release_year FROM sakila.film; 
 -- 5.1
-SELECT COUNT(store_id) FROM sakila.store;
+SELECT COUNT(DISTINCT store_id) FROM sakila.store;
 -- 5.2
-SELECT COUNT(staff_id) FROM sakila.staff;
+SELECT COUNT(DISTINCT staff_id) FROM sakila.staff;
 -- 5.3
-SELECT COUNT(inventory_id) FROM sakila.inventory;
-SELECT COUNT(rental_id) FROM sakila.rental;
+SELECT COUNT(DISTINCT inventory_id) FROM sakila.inventory;
+SELECT COUNT(DISTINCT rental_id) FROM sakila.rental;
 -- 5.4 
 SELECT COUNT(DISTINCT last_name) FROM sakila.actor;
 -- 6
-SELECT length FROM sakila.film ORDER BY length DESC LIMIT 10;
+SELECT title, length FROM sakila.film ORDER BY length DESC LIMIT 10;
 -- 7.1
 SELECT * FROM sakila.actor WHERE first_name = "SCARLETT";
 -- 7.2
